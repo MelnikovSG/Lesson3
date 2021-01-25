7d09632e61281beebfea113affb0196fac3fe0ac
MelnikovSG <57829903+MelnikovSG@users.noreply.github.com>
2021-01-25 23:31:40 +0700

    Homework

---
+++ Lesson 3.2/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace RollBall 
+{ 
+    public class GameController : MonoBehaviour
+    {
+        private InteractiveObject[] _interactiveObjects;
+        private void Awake()
+        {
+            _interactiveObjects = FindObjectsOfType<InteractiveObject>();
+        }
+
+
+        private void Update()
+        {
+            for (var i = 0; i < _interactiveObjects.Length; i++)
+            {
+                var interactiveObject = _interactiveObjects[i];
+                if (interactiveObject == null)
+                {
+                    continue;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ Lesson 3.2/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+namespace RollBall
+{
+    public abstract class InteractiveObject : MonoBehaviour, IInteractable
+    {
+        public bool IsInteractable { get; } = true;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Counter();
+                Interaction();
+                Destroy(gameObject);
+            }
+        }
+
+        protected abstract void Interaction();
+
+        protected abstract void Counter();
+    }
+}
+
