da77202ede55b7f562cbac3e3f9e0e84e3b3585e
MelnikovSG <57829903+MelnikovSG@users.noreply.github.com>
2021-02-04 14:43:26 +0700

    Update GoodBonus.cs

--- Lesson 3.3/Assets/Scripts/GoodBonus.cs
+++ Lesson 3.3/Assets/Scripts/GoodBonus.cs
@@ -3,14 +3,20 @@
 
 namespace RollBall
 {
-    public sealed class GoodBonus : InteractiveObject
+    public sealed class GoodBonus : InteractiveObject, IFlay, IFlicker
     {
+        private Material _material;
+        private float _lengthFlay;
+        
         private DisplayBonuses _displayBonuses;
         int count = 0;
         public UnityEvent HitEvent;
 
         public void Awake()
         {
+            _material = GetComponent<Renderer>().material;
+            _lengthFlay = Random.Range(1.0f, 5.0f);
+
             _displayBonuses = new DisplayBonuses();
         }
 
@@ -19,8 +25,22 @@ namespace RollBall
             _displayBonuses.Display(count);
             HitEvent.Invoke();
         }
+        
+         public void Flay()
+        {
+                transform.localPosition = new Vector3(transform.localPosition.x, 
+                Mathf.PingPong(Time.time, _lengthFlay),
+                transform.localPosition.z);
+        }
+        
+        public void Flicker()
+        {
+            _material.color = new Color(_material.color.r, _material.color.g, _material.color.b,
+                Mathf.PingPong(Time.time, 1.0f));
+        }
 
-        protected override void Counter()
+
+        protected override void Counter() // вывести в отдельный класс
         {
             count++;
         }
