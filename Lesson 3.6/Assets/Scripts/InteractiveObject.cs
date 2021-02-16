6c81a0b0bcd060ebf3ad8a078ae0e7f8512dd8c0
MelnikovSG <57829903+MelnikovSG@users.noreply.github.com>
2021-02-16 23:19:50 +0700

    Homework 6

---
+++ Lesson 3.6/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using static UnityEngine.Random;
+
+namespace Geekbrains
+{
+    public abstract class InteractiveObject : MonoBehaviour, IExecute
+    {
+        protected Color _color;
+
+        private bool _isInteractable;
+
+        protected bool IsInteractable
+        {
+             get { return _isInteractable; }
+             private set
+             {
+                 _isInteractable = value;
+                 GetComponent<Renderer>().enabled = _isInteractable;
+                 GetComponent<Collider>().enabled = _isInteractable;
+             }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsInteractable || !other.CompareTag("Player"))
+            {
+                return;
+            }
+            IsInteractable = false;
+            Interaction();
+        }
+
+        protected abstract void Interaction();
+        public abstract void Execute();
+
+        private void Start()
+        {
+            IsInteractable = true;
+            _color = ColorHSV();
+            if (TryGetComponent(out Renderer renderer))
+            {
+                renderer.material.color = _color;
+            }
+        }
+    }
+}
---
+++ Lesson 3.6/Assets/dataSave/data.bat
@@ -0,0 +1,10 @@
+<Player>
+  <Name value="Roman">
+    <Test X="7,86" Y="1" Z="-11,01" />
+  </Name>
+  <PosX value="7,86" />
+  <PosY value="1" />
+  <PosZ value="-11,01" />
+  <IsEnable value="True" />
+  <Info Unity="2020.1.0f1">System Language: Russian</Info>
+</Player>
